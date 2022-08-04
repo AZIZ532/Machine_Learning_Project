6f16d57ec29d0bc84a4c9e116e250b2579b4ba3a
Abdul Aziz khan <khanabdulaziz532@gmail.com>
2022-08-05 00:46:28 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+from typing import List
+
+
+
+#declaring variables for setup functions
+Project_name = "housing-predictor"
+Version = "0.0.1"
+Author = "Abdul Aziz Khan"
+Description = "This is my first machine learning end to end project"
+Packages = ["housing"]
+Requirement_file_name = "requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement mention in requirements.txt file
+    
+    return the package name which is used to install for this project
+    """
+    
+    with open(Requirement_file_name) as requirement_file:
+        requirement_file.readlines()
+setup (
+    name=Project_name,
+    version=Version,
+    author=Author,
+    description= Description,
+    packages=Packages,
+    install_requires = get_requirements_list()
+)
