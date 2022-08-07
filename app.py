91535edbd72ebc37398a4b3b033a44644c42c0e6
Abdul Aziz khan <khanabdulaziz532@gmail.com>
2022-08-07 22:49:47 +0530

    updated

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 #from crypt import methods
+import sys
 from flask import Flask
+from housing.exception import HousingException
 from housing.logger import logging
-
 app=Flask(__name__)
 
 @app.route("/",methods = ['POST','GET'])
 def index():
-    logging.info("we are testing logging module in logger file !")
-    return "Hello world"
+    try:
+        raise Exception(" we are testing this custom exception")
+    except Exception as e:
+        raise HousingException(e,sys) from e
+        logging.info(housing.error_message)    
+        logging.info("we are testing logging module in logger file !")
+    return "world"
 
 
 if __name__=="__main__":
