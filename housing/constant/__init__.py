c9179cd93c453406148eeac4a820e30b3c9caddd
Abdul Aziz khan <khanabdulaziz532@gmail.com>
2022-08-14 00:10:02 +0530

    updated

---
+++ housing/constant/__init__.py
@@ -0,0 +1,19 @@
+from distutils.command.config import config
+import os
+from datetime import datetime
+ROOT_DIR = os.getcwd()   #to get current working directory
+
+
+CONFIG_DIR = "config"
+CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
+
+CURRUNT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+
+
+#training pipeline  related variable
+
+TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
+TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
+TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
