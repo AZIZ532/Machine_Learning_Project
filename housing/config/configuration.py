7d739a52821544eab3f98926a6c1282c6932a976
Abdul Aziz khan <khanabdulaziz532@gmail.com>
2022-08-15 03:38:14 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -7,8 +7,7 @@ from housing.logger import *
 from housing.util.util import read_yaml_file
 from housing.exception import HousingException
 import os,sys
-import yaml
-
+from housing.constant import * 
 
 class Configuration():
     def __init__(self,
@@ -24,12 +23,29 @@ class Configuration():
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         try:
-            self.config_info
-            data_ingestion_config = DataIngestionConfig(dataset_download_url,
-                                                         tgz_download_dir,
-                                                          raw_data_dir, 
-                                                          ingested_train_dir, 
-                                                          ingested_test_dir) 
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            data_ingestion_artifact_dir = os.path.join(artifact_dir,DATA_INGESTION_ARTIFACT_DIR,self.time_stamp)
+
+            data_ingestion_info = self.config_info[DATA_INGESTION_CONFIG_KEY]
+
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+
+            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
+
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY])
+
+            data_ingested_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY])
+
+            ingested_train_dir = os.path.join(data_ingested_dir,data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY])
+
+            ingested_test_dir = os.path.join(data_ingested_dir,data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY])
+
+            data_ingestion_config = DataIngestionConfig(dataset_download_url = dataset_download_url ,
+                                                         tgz_download_dir = tgz_download_dir,
+                                                          raw_data_dir = raw_data_dir, 
+                                                          ingested_train_dir = ingested_train_dir, 
+                                                          ingested_test_dir = ingested_test_dir) 
             logging.info(f"Data ingestion cofig : {data_ingestion_config}")
             return data_ingestion_config
         except Exception as e:
