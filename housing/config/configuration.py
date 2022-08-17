23f046d8ef0671e48c032b5ce26adb2f77690611
Abdul Aziz khan <khanabdulaziz532@gmail.com>
2022-08-17 13:27:35 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -33,27 +33,32 @@ class Configuration():
 
             dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
 
-            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
+            tgz_download_dir = os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY]
             )
 
-            raw_data_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY]
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY]
             )
 
-            data_ingested_dir = os.path.join(data_ingestion_artifact_dir,data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+            data_ingested_dir = os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
             )
 
-            ingested_train_dir = os.path.join(data_ingested_dir,data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+            ingested_train_dir = os.path.join(data_ingested_dir,
+            data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
             )
 
-            ingested_test_dir = os.path.join(data_ingested_dir,data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+            ingested_test_dir = os.path.join(data_ingested_dir,
+            data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
             )
 
-            data_ingestion_config = DataIngestionConfig(dataset_download_url = dataset_download_url ,
-                                                          tgz_download_dir = tgz_download_dir,
-                                                          raw_data_dir = raw_data_dir, 
-                                                          ingested_train_dir = ingested_train_dir, 
-                                                          ingested_test_dir = ingested_test_dir
-                                                          ) 
+            data_ingestion_config = DataIngestionConfig(dataset_download_url = dataset_download_url,
+            tgz_download_dir = tgz_download_dir,
+            raw_data_dir = raw_data_dir, 
+            ingested_train_dir = ingested_train_dir, 
+            ingested_test_dir = ingested_test_dir
+            ) 
             logging.info(f"Data ingestion cofig : {data_ingestion_config}")
             return data_ingestion_config
         except Exception as e:
@@ -61,9 +66,29 @@ class Configuration():
 
     def get_data_validation_config(self) -> DataValidationConfig:
         try:
-            schema_file_path = None
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            data_validation_artifact_dir = os.path.join(artifact_dir,
+            DATA_VALIDATION_ARTIFACT_DIR_NAME,self.time_stamp
+            )
+
+            schema_file_path = os.path.join(ROOT_DIR,
+            data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
+            data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]]
+            )
+            
+            report_file_path = os.path.join(data_validation_artifact_dir,
+            data_validation_config[DATA_VALIDATION_REPORT_FILE_NAME_KEY]
+            )
+            
+            report_page_file_path = os.path.join(data_validation_artifact_dir,
+            data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY]
+            )
+            
+            
             data_validation_config = DataValidationConfig(
-                    schema_file_path=schema_file_path
+                    schema_file_path=schema_file_path,
+                    report_file_path=report_file_path,
+                    report_page_file_path=report_page_file_path
             )
             return data_validation_config 
         except Exception as e:
