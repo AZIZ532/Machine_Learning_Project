370c4172eb6a0ae12e63a3742634649dd0de9c2e
Abdul Aziz Khan <khanabdulaziz532@gmail.com>
2022-09-11 10:59:34 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -1,6 +1,5 @@
 import logging
 from msilib import schema
-from operator import add
 from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelEvalutionConfig, ModelPusherConfig, ModelTrainerConfig, TrainingPipelineConfig  \
       , ModelEvalutionConfig ,ModelPusherConfig, TrainingPipelineConfig
 from housing.util.util import read_yaml_file
@@ -11,17 +10,18 @@ from housing.exception import HousingException
 import os,sys
 from housing.constant import * 
 
-class Configuration():
+class Configuration:
+
     def __init__(self,
-                config_file_path:str = CONFIG_FILE_PATH,
-                current_time_stamp:str = CURRUNT_TIME_STAMP
-                ) -> None:
-                try:
-                    self.config_info =  read_yaml_file(config_file_path)
-                    self.training_pipeline_config = self.get_training_pipeline_config()
-                    self.time_stamp = current_time_stamp
-                except Exception as e:
-                    raise HousingException(e,sys) from e    
+        config_file_path:str =CONFIG_FILE_PATH,
+        current_time_stamp:str = CURRENT_TIME_STAMP
+        ) -> None:
+        try:
+            self.config_info  = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e    
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         try:
